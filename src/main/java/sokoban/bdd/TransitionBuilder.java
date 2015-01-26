46d2f4a62ad18946f29f1f7a39d07eabebfc8dda
Rafal Jagielski <rafal.jan.jagielski@gmail.com>
2015-01-26 17:42:36 +0100

    Implemented reversed directed transitions.

--- src/main/java/sokoban/bdd/TransitionBuilder.java
+++ src/main/java/sokoban/bdd/TransitionBuilder.java
@@ -22,6 +22,11 @@ public class TransitionBuilder {
 
   private BDD mAllTransitions;
 
+  private BDD mUpTransitionReversed;
+  private BDD mRightTransitionReversed;
+  private BDD mDownTransitionReversed;
+  private BDD mLeftTransitionReversed;
+
   public TransitionBuilder(final BDDBuilder bddBuilder, final Screen screen) {
     mBDDBuilder = bddBuilder;
     mScreen = screen;
@@ -67,6 +72,38 @@ public class TransitionBuilder {
     return mDownTransition;
   }
 
+  public BDD leftTransitionReversed() {
+    if (mLeftTransitionReversed == null) {
+      initAllTransitions();
+    }
+
+    return mLeftTransitionReversed;
+  }
+
+  public BDD upTransitionReversed() {
+    if (mUpTransitionReversed == null) {
+      initAllTransitions();
+    }
+
+    return mUpTransitionReversed;
+  }
+
+  public BDD rightTransitionReversed() {
+    if (mRightTransitionReversed == null) {
+      initAllTransitions();
+    }
+
+    return mRightTransitionReversed;
+  }
+
+  public BDD downTransitionReversed() {
+    if (mDownTransitionReversed == null) {
+      initAllTransitions();
+    }
+
+    return mDownTransitionReversed;
+  }
+
   private void initAllTransitions() {
     mUpTransition = directedTransition(Direction.UP);
     mRightTransition = directedTransition(Direction.RIGHT);
@@ -74,6 +111,11 @@ public class TransitionBuilder {
     mLeftTransition = directedTransition(Direction.LEFT);
 
     mAllTransitions = or(mLeftTransition, mUpTransition, mRightTransition, mDownTransition);
+
+    mUpTransitionReversed = directedTransitionReversed(Direction.UP);
+    mRightTransitionReversed = directedTransitionReversed(Direction.RIGHT);
+    mDownTransitionReversed = directedTransitionReversed(Direction.DOWN);
+    mLeftTransitionReversed = directedTransitionReversed(Direction.LEFT);
   }
 
   private BDD directedTransition(final Direction direction) {
@@ -90,8 +132,8 @@ public class TransitionBuilder {
   private BDD directedOnePointTransition(final int i, final int j, final Direction direction) {
     // TODO make this method smaller
     int[] values = getRowsAndColsForDirection(i, j, direction);
-    int row0 = values[0];
-    int col0 = values[1];
+    int row0 = i;
+    int col0 = j;
     int row1 = values[2];
     int col1 = values[3];
     int row2 = values[4];
@@ -100,11 +142,7 @@ public class TransitionBuilder {
     Field[][] fields = mScreen.getFields();
     BDD noMove = mBDDBuilder.zero();
 
-    if (fields[row0][col0] == Field.WALL) {
-      return noMove;
-    }
-
-    if (fields[row1][col1] == Field.WALL) {
+    if (fields[row0][col0] == Field.WALL || fields[row1][col1] == Field.WALL) {
       return noMove;
     }
 
@@ -139,17 +177,86 @@ public class TransitionBuilder {
     );
   }
 
-  // TODO create a class to be returned
+  private BDD directedTransitionReversed(final Direction direction) {
+    BDD allTransitionsReversed = mBDDBuilder.zero();
+    for (int i = 0; i < mScreen.height(); i++) {
+      for (int j = 0; j < mScreen.width(); j++) {
+        allTransitionsReversed.orWith(directedOnePointTransitionReversed(i, j,
+                direction));
+      }
+    }
+
+    return allTransitionsReversed;
+  }
+
+  private BDD directedOnePointTransitionReversed(final int i, final int j, final Direction direction)
+  {
+    int[] values = getRowsAndColsForDirection(i, j, direction);
+    int rowb1 = values[0];
+    int colb1 = values[1];
+    int row0 = i;
+    int col0 = j;
+    int row1 = values[4];
+    int col1 = values[5];
+
+    Field[][] fields = mScreen.getFields();
+    BDD noMove = mBDDBuilder.zero();
+
+    if (fields[row0][col0] == Field.WALL || fields[rowb1][colb1] == Field.WALL) {
+      return noMove;
+    }
+
+    BDD placeToMoveBackward = mBDDBuilder.varFor(rowb1, colb1);
+    BDD moveManOnly = mBDDBuilder.createStateForMan(row0, col0)
+            .and(mBDDBuilder.createStateForMan(rowb1, colb1).replace(mBDDBuilder.getReversedPairing()))
+            .and(sameBlocks());
+
+    if (fields[row1][col1] == Field.WALL) {
+      return placeToMoveBackward.ite(
+              noMove, //there is a box so man did not come from there
+              moveManOnly// we can move because it is empty
+      );
+    }
+
+    BDD placeWherePotentiallyBoxWasPushed = mBDDBuilder.varFor(row1, col1);
+    Set<Integer> ignore = new HashSet<>();
+    ignore.add(mBDDBuilder.translate(row0, col0, REGULAR));
+    ignore.add(mBDDBuilder.translate(row1, col1, REGULAR));
+    BDD moveManAndBlock = mBDDBuilder.createStateForMan(row0, col0)
+            .and(mBDDBuilder.createStateForMan(rowb1, colb1).replace(mBDDBuilder.getReversedPairing()))
+            .and(mBDDBuilder.negatedPrimaryVarFor(row1, col1))
+            .and(mBDDBuilder.primaryVarFor(row0, col0))
+            .and(sameBlocksExcept(ignore));
+
+    return placeToMoveBackward.ite(
+            noMove,
+            placeWherePotentiallyBoxWasPushed.ite(//if true then there is a box
+              moveManAndBlock, //if true then box was pushed there
+              moveManOnly// if false then there were no box
+    ));
+  }
+
+  /**
+   *
+   * @param i
+   * @param j
+   * @param direction an array where two first elements are indexes of field
+   *                  behind the starting position of a man, next two elements
+   *                  are the indexes of a field where man is moving towards,
+   *                  and last two elements are indexes of field where bax will
+   *                  be moved if th is pushed.
+   * @return
+   */
   private int[] getRowsAndColsForDirection(final int i, final int j, final Direction direction) {
     switch (direction) {
       case DOWN:
-        return new int[]{i, j, i + 1, j, i + 2, j};
+        return new int[]{i-1, j, i + 1, j, i + 2, j};
       case LEFT:
-        return new int[]{i, j, i, j - 1, i, j - 2};
+        return new int[]{i, j+1, i, j - 1, i, j - 2};
       case UP:
-        return new int[]{i, j, i - 1, j, i - 2, j};
+        return new int[]{i+1, j, i - 1, j, i - 2, j};
       case RIGHT:
-        return new int[]{i, j, i, j + 1, i, j + 2};
+        return new int[]{i, j-1, i, j + 1, i, j + 2};
       default:
         throw new IllegalArgumentException("Invalid direction");
     }
